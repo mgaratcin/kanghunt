916388822ce5f155e5670321e80f91a9a8089b81
mgaratcin <159758100+mgaratcin@users.noreply.github.com>
2024-10-24 21:01:15 -0600

    Add files via upload

---
+++ deploy_kangaroos.h
@@ -0,0 +1,20 @@
+//Copyright 2024 MGaratcin//
+
+//All rights reserved.//
+
+//This code is proprietary and confidential. Unauthorized copying, distribution,//
+//modification, or any other use of this code, in whole or in part, is strictly//
+//prohibited. The use of this code without explicit written permission from the//
+//copyright holder is not permitted under any circumstances.//
+
+// deploy_kangaroos.h
+#ifndef DEPLOY_KANGAROOS_H
+#define DEPLOY_KANGAROOS_H
+
+#include <vector>
+#include "secp256k1/Int.h"
+
+// Function declaration for deploying kangaroos
+void deploy_kangaroos(const std::vector<Int>& kangaroo_batch);
+
+#endif // DEPLOY_KANGAROOS_H
