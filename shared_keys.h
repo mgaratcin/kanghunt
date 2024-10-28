9f4fcc82c7668ad67597cf47ddbd5a32afeca185
mgaratcin <159758100+mgaratcin@users.noreply.github.com>
2024-10-28 17:27:24 -0600

    Update shared_keys.h

--- shared_keys.h
+++ shared_keys.h
@@ -2,7 +2,7 @@
 #ifndef SHARED_KEYS_H
 #define SHARED_KEYS_H
 
-#include "Int.h"  // Assuming Int is defined here
+#include "secp256k1/Int.h"  // Assuming Int is defined here
 
 // Declare base_key as extern to share across files
 extern Int base_key;
