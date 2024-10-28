5945e030d11314b8fc7d8356dbb51b10b979a4be
mgaratcin <159758100+mgaratcin@users.noreply.github.com>
2024-10-28 17:24:45 -0600

    Create shared_keys.h

---
+++ shared_keys.h
@@ -0,0 +1,10 @@
+// shared_keys.h
+#ifndef SHARED_KEYS_H
+#define SHARED_KEYS_H
+
+#include "Int.h"  // Assuming Int is defined here
+
+// Declare base_key as extern to share across files
+extern Int base_key;
+
+#endif  // SHARED_KEYS_H
