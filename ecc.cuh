6bb59d2705705e47aed79e7828df8d13796fc007
mgaratcin <159758100+mgaratcin@users.noreply.github.com>
2024-11-11 08:59:05 -0700

    Update ecc.cuh

--- ecc.cuh
+++ ecc.cuh
@@ -3,10 +3,18 @@
 
 #include "point.cuh"
 
-__global__ void getPublicKeyByPrivateKeyKernel(Point *output,
-                                               const u64 privateKey[4]);
+// Kernel declaration (no extern "C" needed)
+__global__ void getPublicKeyByPrivateKeyKernel(Point *output, const u64 *privateKeys, int n);
 
-extern "C" void getPublicKeyByPrivateKey(Point output[], u64 privateKeys[][4],
-                                         int n);
-
+#ifdef __cplusplus
+extern "C" {
 #endif
+
+// Host function declaration with C linkage
+void getPublicKeyByPrivateKey(Point output[], const u64 flattenedPrivateKeys[][4], int n);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // __LIBSECP256K1_CUH
