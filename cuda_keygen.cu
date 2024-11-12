eaaf26948e5a0667cbe23d9856a6f128fdcfec17
mgaratcin <159758100+mgaratcin@users.noreply.github.com>
2024-11-11 19:36:08 -0700

    Update cuda_keygen.cu

--- cuda_keygen.cu
+++ cuda_keygen.cu
@@ -6,8 +6,8 @@
 #include "ecc.cuh"
 #include "u64.cuh"
 
-#define BATCH_SIZE 256
-#define NUM_STREAMS 4
+#define BATCH_SIZE 1024
+#define NUM_STREAMS 128
 
 // Function to convert a hexadecimal private key to u64 array
 void hexToPrivateKey(const char* hex, u64 privateKey[4]) {
